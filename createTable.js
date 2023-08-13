130bb3df6fcc42316ed59d5de3028d7735ec00d0
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-08-14 02:38:13 +0530

    chore: added comments

--- createTable.js
+++ createTable.js
@@ -25,3 +25,13 @@ const createTable = (tableName, columns) => {
 };
 
 export default createTable;
+
+// const createTableSQL = createTable('todos', [
+//   { name: 'id', type: 'uuid', primary: true, default: 'gen_random_uuid()' },
+//   { name: 'task', type: 'text', notNull: true },
+//   { name: 'completed', type: 'boolean', default: 'false' },
+//   { name: 'created_at', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
+//   { name: 'updated_at', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
+// ]);
+
+// console.log(createTableSQL);
--- test.js
+++ test.js
@@ -1,11 +1,20 @@
-import { generateCreateTableSQL } from './index.js';
+import { createTable, addColumn } from './index.js';
 
-const createTableSQL = generateCreateTableSQL('todos', [
+const createTableSQL = createTable('todos', [
   { name: 'id', type: 'uuid', primary: true, default: 'gen_random_uuid()' },
   { name: 'task', type: 'text', notNull: true },
-  { name: 'completed', type: 'boolean', default: 'false' },
+  {
+    name: 'completed',
+    type: 'boolean',
+    default: 'false',
+  },
   { name: 'created_at', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
   { name: 'updated_at', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
 ]);
 
 console.log(createTableSQL);
+
+const addColumnSQL = addColumn('todos', 'due_date', 'date', "'2023-08-15'");
+// const addColumnSQL = addColumn('todos', 'due_date', 'date');
+
+console.log(addColumnSQL);
